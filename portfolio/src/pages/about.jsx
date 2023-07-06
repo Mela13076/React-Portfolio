74f698504d8ba4e81ac321dcbe3ae124a65dab68
Amelia Trevino <81050391+Mela13076@users.noreply.github.com>
2023-07-06 08:13:35 -0500

    Small Changes in Code

--- portfolio/src/pages/about.jsx
+++ portfolio/src/pages/about.jsx
@@ -6,7 +6,49 @@ function About(){
 
     return(
         <div className="about-page">
-            <h2>About Me Page</h2>
+            <h2 className="page-header">Learn About Me!</h2>
+            <div className="content-section">
+                <div className="introduction square">
+                    <h2 className="neon-text">Introduction</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="educational-background square">  
+                    <h2 className="neon-text">Educational Background</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="professional-journey square">
+                    <h2 className="neon-text">Professional Journey</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="technical-skills square">
+                    <h2 className="neon-text">Technical Skills</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="specialization square">
+                    <h2 className="neon-text">Specialization</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+                
+                <div className="achievements square">
+                    <h2 className="neon-text">Achievements</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="personal-interest square">
+                    <h2 className="neon-text">Personal Interest</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+                <div className="future-aspirations square">
+                    <h2 className="neon-text">Future Aspirations</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                </div>
+
+            </div>
         </div>
     )
 }
