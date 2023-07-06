1af8c5c6b9c286631e8ceeca4272202aac5a9faf
Amelia Trevino <81050391+Mela13076@users.noreply.github.com>
2023-07-06 10:30:46 -0500

    About me Updates

--- portfolio/src/pages/about.jsx
+++ portfolio/src/pages/about.jsx
@@ -10,13 +10,27 @@ function About(){
             <div className="content-section">
                 <div className="introduction square">
                     <h2 className="neon-text">Introduction</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
+                    <p>Hello again! My name is Amelia Trevino, and I am a Computer Science student at TAMUK. However, my true passion lies in becoming a full-stack web developer. I have a deep love for technology and a strong drive to solve problems. I am dedicated to continuously learning and expanding my skill set. </p>
+                    <p>On a personal note, I'm an emotional goofball who believes in bringing a positive and lighthearted attitude to everything I do. At the same time, I am responsible and hardworking, always striving to give my best in any endeavor I undertake.</p>
                 </div>
 
                 <div className="educational-background square">  
                     <h2 className="neon-text">Educational Background</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores illum magnam, velit totam quis numquam atque voluptatibus, earum delectus nisi magni, nihil porro ipsum voluptates at in? Laboriosam?</p>
-                </div>
+                    <div className="timeline">
+                        <div className="timeline-point">
+                            <div className="timeline-date">2019</div>
+                            <div className="timeline-description">
+                            Bach
+                            </div>
+                        </div>
+                        <div className="timeline-point">
+                            <div className="timeline-date">2023-2025</div>
+                            <div className="timeline-description">
+                            Master's of Science in Computer Science
+                            </div>
+                        </div>
+                    </div>
+                 </div>
 
                 <div className="professional-journey square">
                     <h2 className="neon-text">Professional Journey</h2>
