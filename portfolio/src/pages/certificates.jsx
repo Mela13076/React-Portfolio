1ad3782f6c82a1320ec495992c3abf98ee84c8d7
Amelia Trevino <81050391+Mela13076@users.noreply.github.com>
2023-07-21 10:20:48 -0500

    Certificate Page Update

--- portfolio/src/pages/certificates.jsx
+++ portfolio/src/pages/certificates.jsx
@@ -7,7 +7,7 @@ import cert5 from '../assets/EE_SD_Certificate.png'
 
 function Certificates(){
 
-    const data = [
+    const certificateData = [
         {
             title: 'Udemy Web Design for Beginners: Real World Coding in HTML & CSS',
             image: cert1,
@@ -16,29 +16,29 @@ function Certificates(){
         {
             title: 'Sololearn JavaScript',
             image: cert2,
-            description: 'this is the description',
+            description: 'I mastered fundamental concepts in JavaScript, including syntax, variables, data types, strings, template literals, conditional statements, control flow statements, and functions. These foundational elements paved the way for my journey as a React developer.',
         }, 
         {
             title: 'CodePath Intermediate Web Development',
             image: cert3,
-            description: 'this is the description',
+            description: 'In an immersive project-based course, I delved into intermediate web development techniques, gaining expertise in various areas. I explored React basics, JSX, functional components, props, state, hooks, events, GitHub fundamentals, form handling, arrays as states, events within nested components, HTTP requests, Async/Await, APIs, component lifecycle, conditional rendering, filter(), map(), React Router, CRUD operations, and website deployment. This comprehensive course provided me with all the tools and knowledge necessary to confidently create my own React websites.',
         }, 
         {
             title: 'First Place Award for Computer Science in Senior Design Project',
             image: cert4,
-            description: 'this is the description',
+            description: 'Achieved first place in the Computer Science division for my Engineering Senior Design project. Developed a full-stack website using React, incorporating advanced concepts like AWS Relational Database Services (RDS) with PostgreSQL. On the front-end, I efficiently executed the code using React, while on the back-end, I leveraged Express.js and Node.js to implement error handling, user authentication, and API requests. This project showcases my proficiency in both front-end and back-end technologies, highlighting my ability to deliver robust and sophisticated web applications.',
         }, 
         {
             title: 'First Place Award for Electrical Engineering in Senior Design Project',
             image: cert5,
-            description: 'this is the description',
+            description: 'Secured first place in the Electrical Engineering division for my Engineering Senior Design project. Spearheaded the development of a cutting-edge device to record pressure data, which was then seamlessly uploaded to a cloud database utilizing AWS Relational Database Services (RDS) with PostgreSQL. The data transmission was skillfully executed using Node-RED, ensuring the seamless flow of information to the database. Subsequently, all the data was harnessed to create a robust and fully functional PERN (PostgreSQL, Express.js, React, Node.js) website, showcasing my prowess in full-stack web development. This project not only showcased my technical acumen but also exemplified my dedication to innovation and problem-solving in the realm of electrical engineering.',
         }, 
     ]
 
     return(
         <div className="certificate-page">
             <h1 className="page-header"> Certificates Earned</h1>
-            {data.map((item, index) => (
+            {certificateData.map((item, index) => (
                 <CertificateComponet key={index} {...item} />
             ))}
         </div>
